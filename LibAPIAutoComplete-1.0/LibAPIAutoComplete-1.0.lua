7b835cf6c9d6f3e8e2612aea764153cc6af756b6
Buds <mrbouyou@gmail.com>
2024-07-04 02:30:31 +0200

    Workaround nil errors when loading PetConstantsDocumentation.lua

--- LibAPIAutoComplete-1.0/LibAPIAutoComplete-1.0.lua
+++ LibAPIAutoComplete-1.0/LibAPIAutoComplete-1.0.lua
@@ -13,6 +13,20 @@ local function LoadBlizzard_APIDocumentation()
     C_AddOns.LoadAddOn(apiAddonName)
   end
   if #APIDocumentation.systems == 0 then
+    -- workaround nil errors when loading PetConstantsDocumentation.lua
+    Constants.PetConsts = Constants.PetConsts or {
+      MAX_STABLE_SLOTS = 200,
+      MAX_SUMMONABLE_PETS = 25,
+      MAX_SUMMONABLE_HUNTER_PETS = 5,
+      NUM_PET_SLOTS_THAT_NEED_LEARNED_SPELL = 5,
+      NUM_PET_SLOTS = 205,
+      EXTRA_PET_STABLE_SLOT = 5,
+      STABLED_PETS_FIRST_SLOT_INDEX = 6
+    }
+    MAX_STABLE_SLOTS = MAX_STABLE_SLOTS or 2
+    NUM_PET_SLOTS_THAT_NEED_LEARNED_SPELL = NUM_PET_SLOTS_THAT_NEED_LEARNED_SPELL or 1
+    EXTRA_PET_STABLE_SLOT = EXTRA_PET_STABLE_SLOT or 0
+    -- end of workaround
     APIDocumentation_LoadUI()
   end
 end
